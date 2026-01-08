862a9090f16e830d3dac160e375bc4db390ba3bb
ThiagoDambroski <92185785+ThiagoDambroski@users.noreply.github.com>
2026-01-08 22:55:12 +0000

    fix bug

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,11 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  base: '/lips-lab-website/', 
-})
+export default defineConfig(() => {
+  const isNetlify = process.env.NETLIFY === "true"; // Netlify sets this automatically
+
+  return {
+    plugins: [react()],
+    base: isNetlify ? "/" : "/lips-lab-website/",
+  };
+});
