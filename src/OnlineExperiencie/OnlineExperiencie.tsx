2af6c0e4789afbb6aad21ee9606badb65dd74b28
ThiagoDambroski <92185785+ThiagoDambroski@users.noreply.github.com>
2026-07-02 23:35:42 +0100

    right css

--- src/OnlineExperiencie/OnlineExperiencie.tsx
+++ src/OnlineExperiencie/OnlineExperiencie.tsx
@@ -7,6 +7,7 @@ import type { TypesOptions } from "../Functions/CreateBatomBox/Types";
 import libsBackground from "../assets/libs back.png";
 import agoraImg from "../assets/agora ribbon.png";
 import libsSite from "../assets/libsDisplaySite.svg";
+import "../styles/ExperiencieAndPrice.css";
 import "../styles/OnlineExperiencie.css";
 
 const CreateBatomBox = lazy(() => import("../Functions/CreateBatomBox/CreateBatomBox"));
